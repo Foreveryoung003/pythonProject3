75178261e1f4b54a0d3be55e02199b4f13311809
gk <gk@adamant.local>
2023-05-25 18:28:58 +0300

    Initial commit

---
+++ main.py
@@ -0,0 +1,50 @@
+# #6.8
+# num = int(input('input number n\n'))
+# num1 = 1
+# while num > num1:
+#     num1 = num1 ** 2
+#     num1 = num1 + 1
+# print('result =', num1)
+# #6.22(г)
+# #Дано натуральное число. Определить:г) сумму его цифр, больших пяти;д) произведение его цифр, больших семи;
+# number = int(input('input number\n'))
+# summa = 0
+# while number > 0:
+#     num = number % 10
+#     if num > 5:
+#     summa = summa + num
+#     number = number // 10
+#
+# print('summa =', summa)
+# #6.22(д)
+# number = int(input('input number\n'))
+# mult = 1
+#     while number > 0:
+#     num = number % 10
+#     if num > 7:
+#     mult = num * mult
+#     number = number // 10
+# print('mult =',mult)
+# #6.23(a)
+# number = int(input('input number n\n'))
+# num1 = int(input('input number a\n'))
+# cnt = 0
+# if num1 > 0 and num1 < 10:
+#     while number > 0:
+#     num = number % 10
+#         if num == num1:
+#         cnt = cnt + 1
+#     number = number // 10
+# print('число а встречается =',cnt)
+# #6.23(в)
+# number = int(input('input number n\n'))
+# num1 = int(input('input number a\n'))
+# summa = 0
+# if num1 > 0 and num1 < 10:
+#     while number > 0:
+#     num = number % 10
+#         if num > num1:
+#         summa = summa + num
+#    number = number // 10
+# print('сумма чисел больших а =',summa)
+
